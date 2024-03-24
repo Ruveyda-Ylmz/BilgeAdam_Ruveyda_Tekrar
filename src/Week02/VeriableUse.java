3bc10565777b50e7b84ef20203aa9cbf8c8f2dde
Ruveyda-Ylmz <ylmzruveyda44@gmail.com>
2024-03-24 12:08:37 +0300

    duzeltme2

--- src/Week02/VeriableUse.java
+++ src/Week02/VeriableUse.java
@@ -3,5 +3,11 @@ package Week02;
 public class VeriableUse {
     public static void main(String[] args) {
         int i;
+        int i1;
+        int i2;
+        int i3;
+        int i4;
+        int i5;
+        int i6;
     }
 }
